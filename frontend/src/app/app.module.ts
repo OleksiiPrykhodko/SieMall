9635290dc55e0c6db3c5bb85ea0365a2708a28d7
Oleksii <o.d.prykhodko@gmail.com>
2024-05-18 12:56:36 +0300

    I've added tothe app.module.ts imports of PrimeNG modules and FormModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
+// Angular modules and components.
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
+// App modules and components.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// PrimeNG modules.
 import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -15,8 +20,12 @@ import { ButtonModule } from 'primeng/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
 
-    ButtonModule
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
